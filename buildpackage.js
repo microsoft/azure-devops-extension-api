e1a8a2513a76b2c1f35aa7937b0ac87a5fbf158b
Alois Klink <alois@mermaidchart.com>
2024-01-09 13:02:15 +0000

    Build an ESM version of this package
    The `node buildpackage.js` script now creates a `./bin/esm` folder,
    which contains an ESM version of this package.
    
    I've disabled minification for this folder, since I don't think it's
    very useful, and it just makes the package larger.
    
    We use https://nodejs.org/api/packages.html#conditional-exports
    to automatically map files to the ESM version when appropriate.
    
    The ESM version will work with most bundlers. However, since it's
    missing file-extensions (e.g. it uses `export * from "./Client";`
    instead of `export * from "./Client.js";`), some stricter ESM programs
    will not yet work with it.
    
    Fixes: #109

--- buildpackage.js
+++ buildpackage.js
@@ -13,13 +13,13 @@ const UglifyES = require("uglify-es");
 (async function() {
 
     // Clean bin directory
-    console.log("# Cleaning bin. Running shelljs rm -rf ./bin");
-    shell.rm("-rf", "./bin");
+    console.log("# Cleaning bin/ and esm/. Running shelljs rm -rf ./bin ./esm");
+    shell.rm("-rf", "./bin", "./esm");
 
     // On Windows, we need `\`, on Unix we need `/`.
     const tsc = path.join("node_modules", ".bin", "tsc");
 
-    // Compile typescript
+    // Compile typescript ./bin folder
     console.log(`# Compiling TypeScript. Executing \`${tsc} -p ./tsconfig.json\`.`);
     try {
         execSync(`${tsc} -p ./tsconfig.json`, {
@@ -31,6 +31,24 @@ const UglifyES = require("uglify-es");
         console.log("ERROR: Failed to build TypeScript.");
         process.exit(1);
     }
+
+    // Compile typescript ./esm folder
+    console.log(`# Compiling TypeScript to ESM. Executing \`${tsc} -p ./tsconfig.esm.json\`.`);
+    try {
+      execSync(`${tsc} -p ./tsconfig.esm.json`, {
+          stdio: [0, 1, 2],
+          shell: true,
+          cwd: __dirname,
+      });
+    } catch (error) {
+        console.log("ERROR: Failed to build TypeScript ESM.");
+        process.exit(1);
+    }
+    console.log("# Creating ./esm/package.json to show files in ./esm are ESM");
+    fs.writeFileSync("./esm/package.json", '{"type": "module"}\n');
+    // Move ./esm folder into ./bin/esm
+    console.log("# Moving ./esm folder to ./bin/esm");
+    fs.renameSync("./esm", "./bin/esm");
 
     // Uglify JavaScript
     console.log("# Minifying JS using the UglifyES API, replacing un-minified files.");
@@ -47,7 +65,7 @@ const UglifyES = require("uglify-es");
     });
 
     for (const file of files) {
-        if (file.includes("node_modules/")) {
+        if (file.includes("node_modules/") || file.includes("esm/")) {
             continue;
         }
         fs.writeFileSync(
